568d676b22d08c643ffc57989d211e4b42a11c0f
Emanuele <Emanuele@LAB-EMANUELE>
2024-09-03 11:02:08 +0200

    Modifica inserimento persona con i giusti validation

--- ProgettoContatti/ProgettoContatti.ENTITY/Persona.cs
+++ ProgettoContatti/ProgettoContatti.ENTITY/Persona.cs
@@ -1,13 +1,20 @@
 ﻿using ProgettoContatti.ENTITY.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProgettoContatti.BL
 {
     public class Persona
     {
         public int IdPersona { get; set; }
+
+        [Required(ErrorMessage = "Inserisci il nome")]
         public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Inserisci il cognome")]
         public string Cognome { get; set; } = string.Empty;
+
         public int Eta { get; set; }
+
         public DateTime DataDiNascita { get; set; } = new DateTime(2000,01,01);
 
         public EGenerePersona Genere { get; set; }
